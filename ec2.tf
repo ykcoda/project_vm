e8b7947cfc17d81fa2b4e3a72f6d66eacc6da41f
Yaw Kyereh-Afreh <yawkyerehdevops@outlook.com>
2025-03-24 13:14:11 +0000

    web server added

--- ec2.tf
+++ ec2.tf
@@ -14,3 +14,11 @@ resource "aws_instance" "db-server" {
     Name = "${var.env}-db-server"
   }
 }
+
+resource "aws_instance" "app-server" {
+  ami           = "ami-0f72881cd8392994c"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "${var.env}-app-server"
+  }
+}
