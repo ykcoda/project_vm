a097002d3853a609b711462a54ad2e70b1f42558
Yaw Kyereh-Afreh <yawkyerehdevops@outlook.com>
2025-03-24 13:08:58 +0000

    deploying dev ec2 instances

--- ec2.tf
+++ ec2.tf
@@ -1,16 +1,16 @@
 
-#resource "aws_instance" "web-server" {
- # ami           = "ami-0f72881cd8392994c"
- # instance_type = "t2.micro"
-  #tags = {
-  #  Name = "web-server"
-  #}
-#}
+resource "aws_instance" "web-server" {
+  ami           = "ami-0f72881cd8392994c"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "${var.env}-web-server"
+  }
+}
 
-#resource "aws_instance" "db-server" {
-#  ami           = "ami-0f72881cd8392994c"
- # instance_type = "t2.micro"
- # tags = {
- #   Name = "db-server"
- # }
-#}
+resource "aws_instance" "db-server" {
+  ami           = "ami-0f72881cd8392994c"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "${var.env}-db-server"
+  }
+}
--- variable.tf
+++ variable.tf
@@ -1,3 +1,7 @@
 variable "project" {
   
 }
+
+variable "env" {
+  
+}
