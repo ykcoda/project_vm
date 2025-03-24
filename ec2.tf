93da8c8349efe085c05e9253eeff8b3cb457d1be
Yaw Kyereh-Afreh <yawkyerehdevops@outlook.com>
2025-03-24 12:57:07 +0000

    new ec2 instance added

--- ec2.tf
+++ ec2.tf
@@ -3,6 +3,14 @@ resource "aws_instance" "web-server" {
   ami           = "ami-0f72881cd8392994c"
   instance_type = "t2.micro"
   tags = {
-    Name = "web-server123"
+    Name = "web-server"
+  }
+}
+
+resource "aws_instance" "db-server" {
+  ami           = "ami-0f72881cd8392994c"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "db-server"
   }
 }
